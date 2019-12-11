b912fa2be694c6168cb17bc9fa2d82843be2cf29
sandra <sandra.h.arriaga@gmail.com>
2019-12-10 18:55:30 -0600

    created component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p>My name is Sandra Herrera. I'm an Information Technology engineer.</p>
+                <p>I'm always looking to learn many things and tools to become a great developer</p>
+            </div>
+        )
+    }
+}
+
+export default App;
